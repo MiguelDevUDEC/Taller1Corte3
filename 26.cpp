753d56f6db742953d59fc45c257374262bfbdb2e
Darwin <121475827+Tuputamae@users.noreply.github.com>
2025-06-12 12:00:23 -0500

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -2,18 +2,21 @@
 // Created by Miguel on 11/06/2025.
 //
 
+#include<stdlib.h>
 #include <iostream>
 using namespace std;
 int main (){
     int cant;
     float media=0,suma=0;
-    cout<<"ingrese la cantidad de est: ";
+    cout<<"ingrese la cantidad de estudiantes: ";
     cin>>cant;
     string nombres[cant];
     float calif[cant];
 
     for (int i=0;i<cant;i++){
-        cout<<"ingrese el nombre del estudiante # "<<i + 1<<" : ";
+    	
+    	system("cls");
+        cout<<endl<<"ingrese el nombre del estudiante # "<<i + 1<<" : ";
         cin>>nombres[i];
         cout<<"ingrese la calificacion obtenida en el examen: ";
         cin>>calif[i];
@@ -21,12 +24,13 @@ int main (){
     }
     media=suma/cant;
 
+	system("cls");
 
     cout<<"media del grupo: "<<media<<endl;
     for (int i=0;i<cant;i++) {
         float diferencia=calif[i]-media;
-        cout<< nombres[i]<<" saco  "<< calif[i];
-        cout <<"Diferencia con la media: "<<diferencia<<endl;
+        cout<<endl<< nombres[i]<<" saco  "<< calif[i];
+        cout<<endl<<"Diferencia con la media: "<<diferencia<<endl;
     }
 
     return 0;
